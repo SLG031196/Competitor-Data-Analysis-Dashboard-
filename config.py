95558dd0fb3069fda2ffe733f33219847359e7cd
Shweta gurap <164475489+SLG031196@users.noreply.github.com>
2025-09-03 18:40:33 +0530

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # config.py
 DB_CONFIG = {
-    "username": "dinerodatateam",
-    "password": "4z3AD47jQdata",
-    "host": "webpagescraperstg.dinerotesting.com",
-    "port": "8001",  #
-    "database": "web_page_scrapper"
+    "username": " ",
+    "password": " ",
+    "host": " ",
+    "port": " ",  #
+    "database": " "
+
 }
